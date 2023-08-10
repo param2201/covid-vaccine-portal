f84756a727792510d09cba663c964c4d1cd81231
Param Mistry <118919249+param2201@users.noreply.github.com>
2023-08-10 21:45:13 +0530

    Add files via upload

---
+++ src/VaccineData.cpp
@@ -0,0 +1,10 @@
+#include "VaccineData.h"
+#include<bits/stdc++.h>
+using namespace std;
+
+VaccineData :: VaccineData(string district, string center, int pin, int slot){
+        this -> district = district;
+        centerName = center;
+        pincode = pin;
+        this -> slot = slot;
+}
